f3d9f7d30f3d2c3b3951c12a76c43be191365799
clintgitahi <clinton@yumkenya.com>
2018-11-05 09:24:58 +0300

    adding react router

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,12 @@
 // start up file for client side app
 import React from "react";
 import ReactDOM from "react-dom";
-import Home from "./components/Home";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes";
 
-ReactDOM.hydrate(<Home />, document.querySelector("#root"));
+ReactDOM.hydrate(
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>,
+  document.querySelector("#root")
+);
