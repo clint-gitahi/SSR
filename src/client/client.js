84e833d88054d035ccc27561f710839a43c44011
clintgitahi <clinton@yumkenya.com>
2018-11-05 17:20:07 +0300

    client axios instance

--- src/client/client.js
+++ src/client/client.js
@@ -6,14 +6,19 @@ import { BrowserRouter } from "react-router-dom";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import axios from "axios";
 import { renderRoutes } from "react-router-config";
 import Routes from "./Routes";
 import reducers from "./reducers";
 
+const axiosInstance = axios.create({
+  baseURL: "/api"
+});
+
 const store = createStore(
   reducers,
   window.INITIAL_STATE,
-  applyMiddleware(thunk)
+  applyMiddleware(thunk.withExtraArgument(axiosInstance))
 );
 
 ReactDOM.hydrate(
